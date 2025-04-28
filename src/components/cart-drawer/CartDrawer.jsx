97d72cd347af56081c6e14be1c4cc3b15a4f7abd
Guido Moras <guidomoras94@gmail.com>
2025-04-28 14:54:57 -0300

    cambios

--- src/components/cart-drawer/CartDrawer.jsx
+++ src/components/cart-drawer/CartDrawer.jsx
@@ -2,6 +2,8 @@ import "./cart-drawer.css";
 import { useCartStore } from "../../store/cartStore";
 import { CiCirclePlus } from "react-icons/ci";
 import { CiCircleMinus } from "react-icons/ci";
+import { FaRegTrashCan } from "react-icons/fa6";
+import { IoIosCloseCircleOutline } from "react-icons/io";
 import {
   ButtonCart,
   ButtonInDec,
@@ -34,49 +36,65 @@ export const CartDrawer = () => {
     <>
       <div className="cart-overlay" onClick={closeCart} />
       <div className={`cart-drawer ${isCartOpen ? "open" : ""}`}>
-        <h2 className="hache__dos">🛒 Carrito</h2>
-        {cart.length === 0 && <p>El carrito está vacío</p>}
-        {cart.map((item) => (
-          <div
-            key={item.id}
-            style={{ marginBottom: "1.5rem" }}
-            className="global__container"
-          >
-            <div className="top__container">
-              <img src={item.image} alt={item.nombre} className="grupo__img" />
-              <h4>{item.nombre}</h4>
-            </div>
-            <div className="bottom__container">
-              <p>Precio: ${item.precio}</p>
-              <div
-                style={{ display: "flex", alignItems: "center", gap: "0.5rem" }}
-              >
-                <ButtonInDec onClick={() => decreaseQuantity(item.id)}>
-                  <CiCircleMinus />
-                </ButtonInDec>
-                <span>{item.cantidad}</span>
-                <ButtonInDec onClick={() => increaseQuantity(item.id)}>
-                  <CiCirclePlus />
-                </ButtonInDec>
-                <button onClick={() => removeFromCart(item.id)}>🗑</button>
+        <div className="btn__carrito__container">
+          <h2 className="hache__dos">🛒 Carrito</h2>
+          <a onClick={closeCart} className="btn">
+            <IoIosCloseCircleOutline className="btn__top" />
+          </a>
+        </div>
+        <div className="cart-products">
+          {cart.length === 0 && <p>El carrito está vacío</p>}
+          {cart.map((item) => (
+            <div
+              key={item.id}
+              style={{ marginBottom: "1.5rem" }}
+              className="global__container"
+            >
+              <div className="top__container">
+                <img
+                  src={item.image}
+                  alt={item.nombre}
+                  className="grupo__img"
+                />
+                <h4>{item.nombre + " " + item.marca}</h4>
               </div>
-              <p>Subtotal: ${item.precio * item.cantidad}</p>
+              <div className="bottom__container">
+                <p>Precio: ${item.precio}</p>
+                <div
+                  style={{
+                    display: "flex",
+                    alignItems: "center",
+                    gap: "0.5rem",
+                  }}
+                >
+                  <ButtonInDec onClick={() => decreaseQuantity(item.id)}>
+                    <CiCircleMinus />
+                  </ButtonInDec>
+                  <span>{item.cantidad}</span>
+                  <ButtonInDec onClick={() => increaseQuantity(item.id)}>
+                    <CiCirclePlus />
+                  </ButtonInDec>
+                  <button onClick={() => removeFromCart(item.id)}>
+                    <FaRegTrashCan />
+                  </button>
+                </div>
+                <p>Subtotal: ${item.precio * item.cantidad}</p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
 
-        {cart.length > 0 && (
-          <>
-            <h3>Total: ${total}</h3>
-            <button onClick={clearCart}>Vaciar carrito</button>
-          </>
-        )}
-
-        <button onClick={closeCart} style={{ marginTop: "1rem" }}>
-          Cerrar
-        </button>
-
-        <ButtonCart onClick={handleAgregar}>Agregar al carrito</ButtonCart>
+        <div className="cart-footer">
+          <h3>Total: ${total}</h3>
+          {cart.length > 0 && (
+            <>
+              <button onClick={clearCart} className="btn">
+                Vaciar carrito
+              </button>
+            </>
+          )}
+          <ButtonCart onClick={handleAgregar}>Finalizar Compra</ButtonCart>
+        </div>
       </div>
     </>
   );
